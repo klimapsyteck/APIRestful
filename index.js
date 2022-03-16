193ed03ef60d778a7981ae5147b37c1e0fb1e488
Kesley Lima <kesleylimasi@gmail.com>
2022-03-15 21:27:47 -0300

    There is Way

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/', (req, res)=> {
     res.json({message: 'Get Here'})
 })
 
+//mongodb+srv://klimapsyteck:Wttn46aI6Hn1f9a8@cluster0.y2tsd.mongodb.net/apirestful?retryWrites=true&w=majority
 
 app.listen(3000, () => {
     console.log('Running')
